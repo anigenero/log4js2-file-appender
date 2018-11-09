3b484b6c0f59383198c1114e7865f0ed1106ad71
Robin Schultz <robin@anigenero.com>
2018-11-09 14:21:31 -0500

    initial commit

---
+++ src/file.appender.ts
@@ -0,0 +1,35 @@
+import { Appender, ILogEvent, LogAppender } from '@log4js2/core';
+import { FileHandler } from './handler/file.handler';
+
+export interface IFileAppenderConfig {
+    fileName: string;
+}
+
+@Appender('File')
+export class FileAppender extends LogAppender<IFileAppenderConfig> {
+
+    private readonly _handler: FileHandler;
+
+    constructor(private readonly _config: IFileAppenderConfig) {
+
+        super(_config);
+
+        if (typeof process === 'undefined') {
+            throw new Error('Cannot use FileAppender in browser mode');
+        } else {
+            this._handler = new FileHandler(_config);
+        }
+
+    }
+
+    /**
+     * Appends the log event
+     * @param {ILogEvent} logEvent
+     */
+    public append(logEvent: ILogEvent) {
+        if (logEvent.level <= this.getLogLevel()) {
+            process.nextTick(() => this._handler.append(this.format(logEvent)));
+        }
+    }
+
+}
